882e57a63f2fabd7861acbc04c95ad2ccaa2af93
Bharath M <mahendrabharath0@gmail.com>
2020-11-24 05:01:26 +0530

    Updates

---
+++ precache-manifest.df3a191bba4b0522bddf4ae7bf6d3a62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d397905bfbe90e19db29c37d96490e2",
+    "url": "/Diagnal/index.html"
+  },
+  {
+    "revision": "797669e99ec604414144",
+    "url": "/Diagnal/static/css/main.2d861424.chunk.css"
+  },
+  {
+    "revision": "47f83d9b7f9d188db32a",
+    "url": "/Diagnal/static/js/2.105ce9e6.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Diagnal/static/js/2.105ce9e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "797669e99ec604414144",
+    "url": "/Diagnal/static/js/main.4bd691b2.chunk.js"
+  },
+  {
+    "revision": "0f2d3ff0d248e9599037",
+    "url": "/Diagnal/static/js/runtime-main.ca3eecb8.js"
+  }
+]);
